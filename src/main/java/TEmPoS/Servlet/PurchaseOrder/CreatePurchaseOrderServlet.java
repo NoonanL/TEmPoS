99369f94066e6c8a5eaf433312fbefd2b5ed69f2
Liam Noonan <contactliamnoonan@gmail.com>
2018-10-24 14:13:52 +0100

    re-implemented PurchaseOrder Servlets

--- src/main/java/TEmPoS/Servlet/PurchaseOrder/CreatePurchaseOrderServlet.java
+++ src/main/java/TEmPoS/Servlet/PurchaseOrder/CreatePurchaseOrderServlet.java
@@ -31,10 +31,8 @@ public class CreatePurchaseOrderServlet extends HttpServlet {
         this.h2PurchaseOrder = h2PurchaseOrder;
         this.h2User = h2User;
 
-        requiredParams.put("productId", "String");
-        requiredParams.put("SKU", "String");
-        requiredParams.put("quantity", "String");
         requiredParams.put("branchId", "String");
+        requiredParams.put("status", "String");
         requiredParams.put("UID", "String");
         requiredParams.put("requestUser", "String");
 
@@ -46,66 +44,62 @@ public class CreatePurchaseOrderServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        /**
-//         * Check request is authorised
-//         */
-//        if (!ValidationFilter.authorizedRequest(request)) {
-//            System.out.println("Unauthorised user request from " + request.getRemoteAddr());
-//            Logger.request("Unauthorised Request: " + request.getSession());
-//            response.sendError((HttpServletResponse.SC_UNAUTHORIZED));
-//        } else {
-//
-//            /**
-//             * Check input is valid
-//             * Must successfully convert to JSON
-//             * Must contain required Parameters
-//             */
-//            JSONObject input = ValidationFilter.isValid(request, requiredParams);
-//            JSONObject responseJson = new JSONObject();
-//
-//            /**
-//             * If Verified input is not null:
-//             */
-//            if (input != null) {
-//
-//                PurchaseOrder newOrder = new PurchaseOrder();
-////                newOrder.setProductId(input.getString("productId"));
-////                newOrder.setSKU(input.getString("SKU"));
-////                //System.out.println(input.getString("quantity"));
-////                newOrder.setQuantity(Integer.parseInt(input.getString("quantity")));
-//                //newOrder.setBranchId(input.getString("branchId"));
-//                //newOrder.setUID(input.getString("UID"));
-//               // newOrder.setStatus("Created");
-//
-//                String requestUser = input.getString("requestUser");
-//
-//                if (h2User.isRegistered(requestUser)) {
-//                    try {
-//                        if (h2PurchaseOrder.existingOrder(newOrder.getUID())) {
-//                            responseJson.put("response", "false");
-//                            responseJson.put("error", "Purchase Order not unique.");
-//                        } else {
-//                            if (h2PurchaseOrder.createPurchaseOrder(newOrder)) {
-//                                responseJson.put("response", "OK");
-//                                responseJson.put("error", "None.");
-//                            } else {
-//                                responseJson.put("response", "false");
-//                                responseJson.put("error", "Failed to create new purchase order.");
-//                            }
-//                        }
-//                    } catch (SQLException e) {
-//                        e.printStackTrace();
-//                    }
-//                }
-//            } else {
-//                responseJson.put("response", "false");
-//                responseJson.put("error", "Missing required fields.");
-//            }
-//
-//            response.setContentType("application/json");
-//            PrintWriter out = response.getWriter();
-//            out.print(responseJson);
-//            out.flush();
-//        }
+        /**
+         * Check request is authorised
+         */
+        if (!ValidationFilter.authorizedRequest(request)) {
+            System.out.println("Unauthorised user request from " + request.getRemoteAddr());
+            Logger.request("Unauthorised Request: " + request.getSession());
+            response.sendError((HttpServletResponse.SC_UNAUTHORIZED));
+        } else {
+
+            /**
+             * Check input is valid
+             * Must successfully convert to JSON
+             * Must contain required Parameters
+             */
+            JSONObject input = ValidationFilter.isValid(request, requiredParams);
+            JSONObject responseJson = new JSONObject();
+
+            /**
+             * If Verified input is not null:
+             */
+            if (input != null) {
+
+                PurchaseOrder newOrder = new PurchaseOrder();
+                newOrder.setBranchId(input.getString("branchId"));
+                newOrder.setUID(input.getString("UID"));
+                newOrder.setStatus(input.getString("status"));
+
+                String requestUser = input.getString("requestUser");
+
+                if (h2User.isRegistered(requestUser)) {
+                    try {
+                        if (h2PurchaseOrder.existingOrder(newOrder.getUID())) {
+                            responseJson.put("response", "false");
+                            responseJson.put("error", "Purchase Order not unique.");
+                        } else {
+                            if (h2PurchaseOrder.createPurchaseOrder(newOrder)) {
+                                responseJson.put("response", "OK");
+                                responseJson.put("error", "None.");
+                            } else {
+                                responseJson.put("response", "false");
+                                responseJson.put("error", "Failed to create new purchase order.");
+                            }
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                responseJson.put("response", "false");
+                responseJson.put("error", "Missing required fields.");
+            }
+
+            response.setContentType("application/json");
+            PrintWriter out = response.getWriter();
+            out.print(responseJson);
+            out.flush();
+        }
     }
 }
